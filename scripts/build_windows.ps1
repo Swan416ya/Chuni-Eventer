940bfd6972afc02ea941e9de5ade162fd054895f
Swan416ya <3447049424@qq.com>
2026-05-18 16:37:48 +0800

    v0.7.5 release

--- scripts/build_windows.ps1
+++ scripts/build_windows.ps1
@@ -1,5 +1,5 @@
 param(
-    [string]$Version = "0.7.1",
+    [string]$Version = "0.7.5",
     [switch]$SkipPyInstaller,
     [Alias("SkipBridge")][switch]$SkipPenguinToolsCli,
     [switch]$SkipCompressonator
